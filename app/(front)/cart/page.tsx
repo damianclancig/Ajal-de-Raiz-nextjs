1179b47a6b40c06c9c5da62a7fdcb0ca565d4e1d
Damián Clancig <damian@clancig.com.ar>
2024-04-17 16:36:12 -0300

    Se agregan metadata.

--- app/(front)/cart/page.tsx
+++ app/(front)/cart/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next'
 import CartDetails from './CartDetails'
+
+export const metadata: Metadata = {
+  title: 'Carrito de compras',
+  description: 'Productos agregados en el carrito de compra.',
+}
 
 const CartPage = () => {
   return <CartDetails />
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,37 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- HTML Meta Tags --> */}
+        <title>Ajal de Raiz</title>
+        <meta name="description" content="Tienda de plantas, suculentas y cactus." />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://ajal-de-raiz-nextjs.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Ajal de Raiz" />
+        <meta property="og:description" content="Tienda de plantas, suculentas y cactus." />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/documents/9999957f-2762-4816-8a7a-8eaaf0428347.jpg?token=xh4IoZMJjVu13tHW83QglYQmf_DZ2lkq2bRolA7xNVg&height=500&width=500&expires=33249364177"
+        />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ajal-de-raiz-nextjs.vercel.app" />
+        <meta property="twitter:url" content="https://ajal-de-raiz-nextjs.vercel.app/" />
+        <meta name="twitter:title" content="Ajal de Raiz" />
+        <meta name="twitter:description" content="Tienda de plantas, suculentas y cactus." />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/documents/9999957f-2762-4816-8a7a-8eaaf0428347.jpg?token=xh4IoZMJjVu13tHW83QglYQmf_DZ2lkq2bRolA7xNVg&height=500&width=500&expires=33249364177"
+        />
+
+        {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
+      </head>
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
-          <Header />
+          <Header></Header>
           {children}
           <footer className="footer footer-center p-4 bg-base-300 text-base-content">
             <p>Copyright 2024 - All right reserved by Ajal de Raiz.</p>
