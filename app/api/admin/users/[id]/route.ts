4737fb0b400bd7050a35f41c93e96975ae537e49
Damián Clancig <damian@clancig.com.ar>
2024-04-20 21:25:05 -0300

    Se agrega pantalla editar usuario.

--- app/api/admin/users/[id]/route.ts
+++ app/api/admin/users/[id]/route.ts
@@ -2,6 +2,74 @@ import { auth } from '@/lib/auth'
 import UserModel from '@/lib/models/UserModel'
 import dbConnect from '@/lib/dbConnect'
 
+export const GET = auth(async (...args: any) => {
+  const [req, { params }] = args
+  if (!req.auth) {
+    return Response.json(
+      { message: 'unauthorized' },
+      {
+        status: 401,
+      }
+    )
+  }
+  await dbConnect()
+  const user = await UserModel.findById(params.id)
+  if (!user) {
+    return Response.json(
+      { message: 'Usuario no encontrado' },
+      {
+        status: 404,
+      }
+    )
+  }
+  return Response.json(user)
+}) as any
+
+export const PUT = auth(async (...p: any) => {
+  const [req, { params }] = p
+  if (!req.auth) {
+    return Response.json(
+      { message: 'unauthorized' },
+      {
+        status: 401,
+      }
+    )
+  }
+
+  const { name, email, isAdmin } = await req.json()
+
+  try {
+    await dbConnect()
+
+    const user = await UserModel.findById(params.id)
+    if (user) {
+      user.name = name
+      user.email = email
+      user.isAdmin = Boolean(isAdmin)
+
+      const updatedUser = await user.save()
+      return Response.json({
+        message: 'Usuario actualizado',
+        user: updatedUser,
+      })
+    } else {
+      return Response.json(
+        { message: 'Usuario no encontrado' },
+        {
+          status: 404,
+        }
+      )
+    }
+  } catch (err: any) {
+    return Response.json(
+      { message: err.message },
+      {
+        status: 500,
+      }
+    )
+  }
+}) as any
+
 export const DELETE = auth(async (...args: any) => {
   const [req, { params }] = args
   if (!req.auth) {
