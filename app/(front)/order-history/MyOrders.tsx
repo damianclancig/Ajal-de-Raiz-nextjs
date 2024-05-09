f62bca1552b567acc039a42a1404df3f1fa130b3
Damián Clancig <damian@clancig.com.ar>
2024-05-08 23:27:18 -0300

    Fondo translúcido al titulo y mejoras visuales.

--- app/(front)/order-history/MyOrders.tsx
+++ app/(front)/order-history/MyOrders.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Order } from '@/lib/models/OrderModel'
+import { formatCurrency } from '@/lib/utils'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
@@ -23,47 +24,52 @@ const MyOrders = () => {
       </div>
     )
   return (
-    <div className="overflow-x-auto">
-      <table className="table table-zebra bg-base-100">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>FECHA</th>
-            <th>TOTAL</th>
-            <th>PAGADO</th>
-            <th>ENVIADO</th>
-            <th>ACCIÓN</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order: Order) => (
-            <tr key={order._id}>
-              <td>...{order._id.substring(20, 24)}</td>
-              <td>{order.createdAt.substring(0, 10)}</td>
-              <td>$ {order.totalPrice}</td>
-              <td>
-                {order.isPaid && order.paidAt ? `${order.paidAt.substring(0, 10)}` : 'Pendiente'}
-              </td>
-              <td>
-                {order.isDelivered && order.deliveredAt
-                  ? `${order.deliveredAt.substring(0, 10)}`
-                  : 'Pendiente'}
-              </td>
-              <td>
-                <Link
-                  href={`/order/${order._id}`}
-                  passHref
-                  type="button"
-                  className="btn btn-outline btn-sm"
-                >
-                  Detalles
-                </Link>
-              </td>
+    <>
+      <h2 className="text-3xl p-2 my-2 text-center bg-base-300 bg-opacity-20 backdrop-blur shadow-xl rounded-xl">
+        Mis pedidos
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra bg-base-100">
+          <thead>
+            <tr className="text-lg">
+              <th>ID</th>
+              <th>Fecha</th>
+              <th className="w-24">Total</th>
+              <th>Pagado</th>
+              <th>Enviado</th>
+              <th>Acción</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {orders.map((order: Order) => (
+              <tr key={order._id} className="text-nowrap text-base md:text-sm">
+                <td>...{order._id.substring(20, 24)}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td className="text-right">{formatCurrency(order.totalPrice)}</td>
+                <td>
+                  {order.isPaid && order.paidAt ? `${order.paidAt.substring(0, 10)}` : 'Pendiente'}
+                </td>
+                <td>
+                  {order.isDelivered && order.deliveredAt
+                    ? `${order.deliveredAt.substring(0, 10)}`
+                    : 'Pendiente'}
+                </td>
+                <td>
+                  <Link
+                    href={`/order/${order._id}`}
+                    passHref
+                    type="button"
+                    className="btn btn-outline btn-sm"
+                  >
+                    Detalles
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 export default MyOrders
